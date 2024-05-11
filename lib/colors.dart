346f631e77b512282aef60213537bb7966d1ab11
wanshmain <wanshmain@gmail.com>
2024-05-11 14:08:10 +0530

    google button for sign in

---
+++ lib/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kwhitecolor =Colors.white;
+const kblack=Colors.black;
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,30 @@
+import 'package:docs_clone_flutter/colors.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () {},
+          icon: Image.asset(
+            'assets/images/g-logo-2.png',
+            height: 20,
+          ),
+          label: const Text(
+            'Sign in with Google',
+            style: TextStyle(
+              color: kblack,
+            ),
+          ),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kwhitecolor,
+            minimumSize: const Size(150, 50)
+          ),
+        ),
+      ),
+    );
+  }
+}
